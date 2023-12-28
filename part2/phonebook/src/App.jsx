9fc01d027c829ec35d70b416b0557799891d61e4
Maurice Pruna <8557014+mauricepruna@users.noreply.github.com>
2023-12-28 16:19:33 -0500

    Adding part2 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filteredName, setFilteredName] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get("http://localhost:3001/persons");
+      setPersons(response.data);
+    }
+    fetchData();
+  }, []);
   const addPerson = (event) => {
     event.preventDefault();
     const check_persons = persons.filter((element) => element.name === newName);
