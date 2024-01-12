6eaa9af98f48be81faf47b7fd8903ced36003525
Maurice Pruna <8557014+mauricepruna@users.noreply.github.com>
2024-01-11 23:53:31 -0500

    Adding part3 Exercises 3.7-3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
+morgan.token('body', function (req, res) { 
+  return req.method=='POST' ? JSON.stringify(req.body) : " "
+  
+})
+
 app.use(express.json());
+
+// app.use(morgan("tiny")); //:method :url :status :res[content-length] - :response-time ms
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 let persons = [
   {
